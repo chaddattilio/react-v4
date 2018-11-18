e80b55eb1179bd6cc5a653998b0c5bbb087f7bfc
Chad Dattilio <cdattilio@lyonscg.com>
2018-11-18 14:01:00 -0500

    Adding in petfinder API

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { render } from "react-dom";
+import pf from "petfinder-client";
+import Pet from "./Pet";
+
+const petfinder = pf({
+  key: process.env.API_KEY,
+  secret: process.env.API_SECRET
+});
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      pets: []
+    };
+  }
+  componentDidMount() {
+    petfinder.pet
+      .find({ output: "full", location: "Canton, MI" })
+      .then(data => {
+        let pets;
+
+        if (data.petfinder.pets && data.petfinder.pets.pet) {
+          if (Array.isArray(data.petfinder.pets.pet)) {
+            pets = data.petfinder.pets.pet;
+          } else {
+            pets = [data.petfinder.pets.pet];
+          }
+        } else {
+          pets = [];
+        }
+
+        this.setState({
+          pets: pets
+        });
+      });
+  }
+  render() {
+    return (
+      <div>
+        <h1>Adopt Me!</h1>
+        <pre>
+          <code>{JSON.stringify(this.state, null, 4)}</code>
+        </pre>
+      </div>
+    );
+  }
+}
+
+render(<App />, document.getElementById("root"));
